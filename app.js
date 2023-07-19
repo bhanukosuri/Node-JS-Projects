fbba9fa4f7d382476fba45fad9abae61b84bc830
bhanukosuri <61044101+bhanukosuri@users.noreply.github.com>
2023-07-19 22:10:01 +0530

    After redirecting to home, about, node pages

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    // console.log(req)
-    console.log('Bhanu Prakash')
+    res.setHeader('Content-Type', 'text/html')
+    const url = req.url
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+        res.write('</html>')
+        res.end()
+    } else if (url === '/home'.toLowerCase()) {
+        res.write('<html>')
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('</html>')
+        res.end()
+    } else if (url === '/about'.toLowerCase()) {
+        res.write('<html>')
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</html>')
+        res.end()
+    } else if (url === '/node'.toLowerCase()) {
+        res.write('<html>')
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
 })
 
 server.listen(4000)
