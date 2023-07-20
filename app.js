6047f2abf94345fba991cc90806119b158d92af7
bhanukosuri <61044101+bhanukosuri@users.noreply.github.com>
2023-07-20 18:23:38 +0530

    Clearing and moving the code to another file

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    const url = req.url
-    if (url === '/') {
-        res.write('<html>')
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
-        res.write('</html>')
-        res.end()
-    } else if (url === '/home'.toLowerCase()) {
-        res.write('<html>')
-        res.write('<body><h1>Welcome home</h1></body>')
-        res.write('</html>')
-        res.end()
-    } else if (url === '/about'.toLowerCase()) {
-        res.write('<html>')
-        res.write('<body><h1>Welcome to About Us page</h1></body>')
-        res.write('</html>')
-        res.end()
-    } else if (url === '/node'.toLowerCase()) {
-        res.write('<html>')
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
-        res.write('</html>')
-        res.end()
-    }
-})
+const routes = require('./routes')
 
-server.listen(4000)
+const { parse } = require('path')
+const requestHandler = require('./routes')
+
+// const server = http.createServer(routes)
+
+console.log(routes.someText)
+const server = http.createServer(routes.handler)
+
+
+server.listen(3000)
+
