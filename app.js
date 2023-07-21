e6f9649fc1114f9be3e707e45b8bbd6d3a715429
bhanukosuri <61044101+bhanukosuri@users.noreply.github.com>
2023-07-21 14:22:41 +0530

    After adding 2 input text types to the form

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('In the middlewear')
-    next()
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product', (req, res, next) => {
+    // console.log('This always runs!')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next) => {
-    console.log('In another middlewear')
+app.post('/product', (req, res, next) => {
+    console.log(req.body)    
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    // console.log('In another middlewear')
     res.send('<h1>Hello from Express</h1>')
     // res.send({ "name": "Bhanu" })
 })
 
 app.listen(3000)
-
